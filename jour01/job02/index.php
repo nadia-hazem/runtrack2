10155618d4721b517998572b2af952f9944f4659
nedh <nedh.design@gmail.com>
2022-11-14 15:38:32 +0100

    jour01/corrections

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -7,8 +7,11 @@
     <title>Jour01 - job02</title>
 </head>
 <body>
-    <?php echo 'Hello LaPlateforme!' /*** premier commentaire ***/ ?>
-    <br>
-    <?php echo 'Hello LaPlateforme!' /*** second commentaire ***/ ?>
+    <?php
+    echo 'Hello LaPlateforme ! '; // Ceci est un commentaire sur une seule ligne
+    echo 'Hello LaPlateforme ! ';  /* Ceci est un commentaire sur
+                                    plusieurs lignes */
+    echo 'Hello LaPlateforme ! '; # Ceci est un commentaire style shell sur une seule ligne
+?>
 </body>
 </html>
