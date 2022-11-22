d5f28ca40e07678babdae0182a69a590a6da4726
nedh <nedh.design@gmail.com>
2022-11-22 11:01:49 +0100

    job03 done

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <?php
-function hello() {
-    echo "Hello LaPlateforme!";
-}
 
+function getHello() {
+    return "Hello LaPlateforme!";
+}
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour07 Job 01 - Hello LaPlateforme</title>
+    <title>Jour07 Job 03 - GetHello</title>
 </head>
 <body>
-    <?= hello() ?>
+
+    <?php
+    $str = getHello();
+    echo $str;    
+    ?>
 </body>
 </html>
