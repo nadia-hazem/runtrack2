ec79a04edb1d889c317acd873a734982361ac7d5
nedh <nedh.design@gmail.com>
2022-11-22 10:34:09 +0100

    job01 done

---
+++ jour07/job01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+function hello() {
+    echo "Hello LaPlateforme!";
+}
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello LaPlateforme</title>
+</head>
+<body>
+    <?= hello() ?>
+</body>
+</html>
