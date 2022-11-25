dfb25cf5998c7b17a6de8e07454ad10d2c57b8c7
nedh <nedh.design@gmail.com>
2022-11-25 11:15:03 +0100

    job03 done

--- jour10/job01/index.php
+++ jour10/job01/index.php
@@ -21,6 +21,11 @@ $request = $mysqli->query("select * from etudiants");
     </style>
 </head>
 <body>
+    <!--En utilisant php et mysqli, connectez-vous à la base de données “jour09”. A l’aide d’une
+requête SQL, récupérez l’ensemble des informations de la table etudiants. Affichez le
+résultat de cette requête dans un tableau html. La première ligne de votre tableau html
+(thead) doit contenir le nom des champs. Les suivantes (tbody) doivent contenir les
+données présentes dans votre base de données.-->
     <table>
         <thead>
             <tr>
