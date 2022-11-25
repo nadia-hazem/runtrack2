b725243675df9e11122c3736addf077becf28e5a
nedh <nedh.design@gmail.com>
2022-11-25 11:31:27 +0100

    job06 done

---
+++ jour10/job06/index.php
@@ -0,0 +1,47 @@
+<?php
+
+$mysqli = new mysqli("localhost", "root", "", "jour09");
+
+$request = $mysqli->query("select count(*) from `etudiants`;");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour10 job05</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<!--En utilisant php, connectez-vous à la base de données “jour09”. A l’aide d’une requête
+SQL, récupérez le nombre total d’étudiants dans une colonne nommée “nb_etudiants”.
+Affichez le résultat de cette requête dans un tableau html. La première ligne de votre
+tableau html doit contenir le nom du champ.-->
+        <table>
+            <thead>
+                <tr>
+                    <th>nb_etudiants</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                    while(($resultat = $request->fetch_array()) != null){
+                        echo "<tr>";
+                        echo "<td>".$resultat["count(*)"]."</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+</body>
+</html>
