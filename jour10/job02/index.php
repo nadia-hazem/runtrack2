db4223e399c9bcb274838b464f454133c7dc46d9
nedh <nedh.design@gmail.com>
2022-11-25 10:57:55 +0100

    job02 done

--- jour10/job02/index.php
+++ jour10/job02/index.php
@@ -2,16 +2,17 @@
 
 $mysqli = new mysqli("localhost", "root", "", "jour09");
 
-$request = $mysqli->query("select * from etudiants");
+$request = $mysqli->query("select * from salles");
 
 ?>
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour10 job01</title>
+    <title>Jour10 job02</title>
     <style>
         table, th, td {
             border: 1px solid black;
@@ -21,14 +22,16 @@ $request = $mysqli->query("select * from etudiants");
     </style>
 </head>
 <body>
+<!--En utilisant php et mysqli, connectez-vous à la base de données “jour09”. A l’aide d’une
+requête SQL, récupérez le nom et la capacité de chacune des salles. Affichez le résultat
+de cette requête dans un tableau html. La première ligne de votre tableau html doit
+contenir le nom des champs. Les suivantes doivent contenir les données présentes
+dans votre base de données.-->
     <table>
         <thead>
             <tr>
-                <th>prenom</th>
                 <th>nom</th>
-                <th>naissance</th>
-                <th>sexe</th>
-                <th>email</th>
+                <th>capacite</th>
             </tr>
         </thead>
         <tbody>
@@ -36,15 +39,13 @@ $request = $mysqli->query("select * from etudiants");
 
                 while(($resultat = $request->fetch_array()) != null){
                     echo "<tr>";
-                    echo "<td>".$resultat["prenom"]."</td>";
                     echo "<td>".$resultat["nom"]."</td>";
-                    echo "<td>".$resultat["naissance"]."</td>";
-                    echo "<td>".$resultat["sexe"]."</td>";
-                    echo "<td>".$resultat["email"]."</td>";
+                    echo "<td>".$resultat["capacite"]."</td>";
                     echo "</tr>";
                 }
             ?>
         </tbody>
     </table>
+
 </body>
 </html>
