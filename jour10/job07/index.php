11b6d127d0ac0a875496a9ab018ac3a0f52be9fc
nedh <nedh.design@gmail.com>
2022-11-25 11:35:58 +0100

    job07 done

--- jour10/job07/index.php
+++ jour10/job07/index.php
@@ -2,7 +2,7 @@
 
 $mysqli = new mysqli("localhost", "root", "", "jour09");
 
-$request = $mysqli->query("select count(*) from `etudiants`;");
+$request = $mysqli->query("select  sum(`superficie`) from `etage`;");
 
 ?>
 
@@ -12,7 +12,7 @@ $request = $mysqli->query("select count(*) from `etudiants`;");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour10 job06</title>
+    <title>Jour10 job07</title>
     <style>
         table, th, td {
             border: 1px solid black;
@@ -23,13 +23,14 @@ $request = $mysqli->query("select count(*) from `etudiants`;");
 </head>
 <body>
 <!--En utilisant php, connectez-vous à la base de données “jour09”. A l’aide d’une requête
-SQL, récupérez le nombre total d’étudiants dans une colonne nommée “nb_etudiants”.
-Affichez le résultat de cette requête dans un tableau html. La première ligne de votre
-tableau html doit contenir le nom du champ.-->
+SQL, récupérez la superficie totale des étages dans une colonne nommée
+“superficie_totale”. Affichez le résultat de cette requête dans un tableau html. La
+première ligne de votre tableau html doit contenir le nom des champs. Les suivantes
+doivent contenir les données présentes dans votre base de données.-->
         <table>
             <thead>
                 <tr>
-                    <th>nb_etudiants</th>
+                    <th>superficie_totale</th>
                 </tr>
             </thead>
             <tbody>
@@ -37,7 +38,7 @@ tableau html doit contenir le nom du champ.-->
 
                     while(($resultat = $request->fetch_array()) != null){
                         echo "<tr>";
-                        echo "<td>".$resultat["count(*)"]."</td>";
+                        echo "<td>".$resultat["sum(`superficie`)"]."</td>";
                         echo "</tr>";
                     }
                 ?>
